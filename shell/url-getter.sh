7c0b7234d9916c0aef5ed4d1f53cc69729a82342
felipe <luizfelipetx@gmail.com>
2016-12-12 22:21:22 -0200

    ajustando paths

--- shell/url-getter.sh
+++ shell/url-getter.sh
@@ -13,7 +13,7 @@ addlog(){
         echo "getdata ? $1" >> logrequest.log
 }
 
-for url in $(cat urls.txt);do
-        wget --spider -S "${url}" 2>&1 | grep "HTTP/" | awk -v date="$(date +"%Y-%m-%d %r")"   '{print $2 ,"---", date }' >>logrequest.log
+for url in $(cat /opt/cron/urls.txt);do
+        wget --spider -S "${url}" 2>&1 | grep "HTTP/" | awk -v date="$(date +"%Y-%m-%d %r")"   '{print $2 ,"---", date }' >>/opt/cron/logrequest.log
 done
-echo "last run $(date)" >> logcron.log
+echo "last run $(date)" >> /opt/cron/logcron.log
